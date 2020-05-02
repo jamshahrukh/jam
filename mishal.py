492db44c3788b9d6ea1d53efbf71c7791e2b3af9
Jam Shahrukh <64614842+jamshahrukh@users.noreply.github.com>
2020-05-02 06:38:07 +0500

    Update mishal.py

--- mishal.py
+++ mishal.py
@@ -396,7 +396,7 @@ def pilih_super():
 																oks.append(user+pass7)
 															else:
 																if 'www.facebook.com' in q["error_msg"]:
-																	print ('\x1b[1);96m[\x1b[1;93mCheckpoint\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7
+																else:	print ('\x1b[1);96m[\x1b[1;93mCheckpoint\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7
 																	cek = open("out/checkpoint.txt", "a")
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
